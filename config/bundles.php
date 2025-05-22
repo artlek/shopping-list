ec3db202c509612545bb9ea47aabe43fe3ad616e
Artur Lekston <artur.lekston@gmail.com>
2025-05-22 17:11:02 +0200

    edit bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -5,7 +5,7 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     // Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => false, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => false, 'test' => false],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
