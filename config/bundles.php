a6f2bc07d12dca83ac93e23ee4cac8dc3f94c96f
Artur Lekston <artur.lekston@gmail.com>
2025-05-22 17:14:33 +0200

    edit bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -7,7 +7,7 @@ return [
     // Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => false, 'test' => false],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => false, 'test' => false],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
